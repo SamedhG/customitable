73d55211ab3bcc36a530b783ad35c23b06f97f95
SamedhG <gupta.sam@husky.neu.edu>
2021-03-21 17:26:36 -0400

    Added example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import 'Table' from './table/Table';
+
+
+function DummyComponent({row, id}) {
+    return(<p>{row["id"]}</p>)
+}
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+    let columns = [{
+        id: "name",
+        component: DummyComponent,
+    },
+    {
+        id: "age",
+        component: DummyComponent,
+    }]];
+    let rows = [{
+        name: "Alice",
+        age: 20
+    },
+    {
+        name: "Bob",
+        age: 21
+    }];
+    return (
+        <div className="App">
+            <Table columns={columns} rows={rows}/>
+        </div>
+    );
 }
 
 export default App;
